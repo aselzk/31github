a1ea9bc0a4c89df7cc984e170494b02b80e1978a
aselzk <150859616+aselzk@users.noreply.github.com>
2023-11-15 00:44:17 -0600

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,41 @@
+print("You can do it! Just do it!")
+
+# print - is a Function
+# functions can be: 
+#1. built-in Python Functions or 
+#2. modules functions 
+#3.your own code 
+
+# ("You can do it! Just do it!") - is an Argument 
+# - the value btw quotes is a string
+
+# Additional info:
+#  to print the arguments one after another (otherwise all of the arguments will be in one line) -> ;\
+print("You can do it!");\
+print("Just do it!")
+
+#  to separate the long sentences -> \n
+print("You can do it! \n Just do it!")
+
+#if we separate using comma (,) python will print it all in one line
+print("You", "can", "do", "it!", "Just do it!")
+
+#1.keyword arguments -> end= -> will print all in the same line
+print("You can do it!", end="");\
+print("Just do it!")
+
+#can specify values/characters in end= -> will print all in the same line
+print("You can do it", end="!!!");\
+print("Just do it!")
+
+#2. keyword arguments -> sep= -> to separate the words (can pass some characters)
+print("You", "can", "do", "it!", "Just do it!", sep=":)")
+
+print('My age is ' + 25)
+
+
+
+
+
+
+

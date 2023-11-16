bea74ecb62562faca9164ee1e754da7c9b0bfae4
aselzk <150859616+aselzk@users.noreply.github.com>
2023-11-16 02:13:13 -0600

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,70 @@
+#Literals 
+# 1. Integers(whole numbers, no fractions): Octal numbers(0o123)/ Hexadecimal numbers (0x123)
+# 2. Floating point numbers (numbers w/ fractions ex:-90.0  89.394)
+# 3. Strings: (text: "Hello!" or 'Hello!')
+# 4. Booleans: (true, false)
+
+#Operators
+# 7 types of Arithmetic Operators:
+#    1. Exponential: base and exponents -> 2**3
+print(2**3)
+print(2.**3.)
+print(2.**3)
+#    2. Multiplication (single asterisk)
+print(2*3)
+print(2.*3.)
+print(2.*3)
+#    3. Division (always returns float)
+print(10/2)
+print(10./2.)
+print(10./2)
+
+#    4. Floor division: in order to return integer, also it rounds the numbers
+print(10//2)
+print(10.//2.)
+print(10.//2)
+
+print(6/-4)
+print(6//-4)
+
+#    5. Modulo: to calculate the remainder after division
+print(4%2)
+print(5%2)
+
+#    6. Addition:
+print(4+2)
+print(5+2)
+
+#    7. Substraction:
+print(4-2)
+print(5-2)
+
+
+#Binary operator: 2-2
+#Unary operator: -2
+
+#quiz:
+print(13/4+13%4)
+print(2*(2+3))
+print(2 * 3 ** 3 * 4)
+print(6. // 4)
+print(9 % 4)
+
+x = 10 / 4
+y = 5 / 2.0
+print (x + y)
+
+print(10 - 6 ** 2 / 9 * 10 + 1)
+
+
+
+
+
+
+
+
+
+
+
+
+

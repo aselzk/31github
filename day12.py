ca0bd5d0b4311bf83803fef5becfae92a3933869
aselzk <150859616+aselzk@users.noreply.github.com>
2023-11-26 02:03:16 -0600

    Create day12.py

---
+++ day12.py
@@ -0,0 +1,9 @@
+#finding lists
+#element in list
+#element not in list
+
+letters = ["A", "B", "C", "D", "E"]
+print("B" in letters)
+
+letters = ["A", "B", "C", "D", "E"]
+print("Z" not in letters)

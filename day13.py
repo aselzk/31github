f05dc2be5b48216377a9a1fda2e8670ffff079e6
aselzk <150859616+aselzk@users.noreply.github.com>
2023-11-27 03:15:31 -0600

    Create day13.py

---
+++ day13.py
@@ -0,0 +1,15 @@
+#Nested Lists - 2D array is the list which consists the other lists
+#Matrix
+
+classroom = [
+  ["Sam", "Max", "Joe", "Anne"],
+  ["Sofie", "Lisa", "Tim", "Sasha",
+  ["Claire", "Sara", "Leo", "Kim"],
+  ["Zoe", "Guy", "Anna", "Eva"], 
+]
+
+#to get the name from the nested lists
+student = classroom[2][1]
+print(student)
+
+#Nested Lists - 2D array is the list which consists the other lists

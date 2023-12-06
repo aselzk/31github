0f6db9770b1d2ddd5661718a6c5f044c3d4f7c29
aselzk <150859616+aselzk@users.noreply.github.com>
2023-12-06 00:41:01 -0600

    Create day22.py
    Python print/input/variables

---
+++ day22.py
@@ -0,0 +1,19 @@
+#UDEMY COURSE WITH ANGELA
+#100 days of code Challenge
+# Write your code below this line 👇
+#String is a text will be colored orange and inside the double quotes
+#Print function
+print("Hello World!\nHello World!")
+print("Hello" + " Asel")
+
+#Input function
+input("Whats your name?")
+print("Hello " + input("Whats your name?"))
+
+#already placed inputs
+name1 = input("What is your name? ")
+print(name1) #This line will print the text and line 1 of the input into the output.
+name2 = input()
+print(name2) #This line will only print the name on line 2 of the input pane.
+
+#tomorrow I will finish day1 and do day2 with Angela

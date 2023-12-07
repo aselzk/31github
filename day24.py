a21149294b4f4cca016bd37a9d5e7138bc4918a7
aselzk <150859616+aselzk@users.noreply.github.com>
2023-12-07 15:05:56 -0600

    Create day24.py

---
+++ day24.py
@@ -0,0 +1,10 @@
+#2/100 - continuation of previous lesson
+#Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
+two_digit_number = input("Your two numbers?\n")
+print(two_digit_number)
+print(type(two_digit_number)) #Type check
+
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+two_digit_number = first_digit + second_digit
+print(two_digit_number)
